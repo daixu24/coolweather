6156077168e90082e7c877feefb2afa02a999ef1
daixu24 <820259765@qq.com>
2022-09-16 15:33:10 +0800

    config

---
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -0,0 +1,41 @@
+package com.example.coolweather.db;
+
+public class Country {
+
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+}
